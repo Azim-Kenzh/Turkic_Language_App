d510715f941f9b60127adc6e258f4d12a5b74e83
Azim <azimkozho@gmail.com>
2021-07-27 16:39:02 +0600

    updadte

--- account/serializers.py
+++ account/serializers.py
@@ -1,13 +1,6 @@
-import self
-from django.contrib.auth import authenticate
-from django.contrib.sitemaps.views import index
-from django.db.models import Count
 from rest_framework import serializers
-from rest_framework.authtoken.serializers import AuthTokenSerializer
 
 from account.models import *
-from app.models import Favorite
-from app.serializers import FavoriteSerializer
 
 
 class RegisterSerializer(serializers.ModelSerializer):
